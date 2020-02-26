fde5dc43730174f88f7483039a1da96ea659ca3a
= <farhapartex@gmail.com>
2020-02-26 15:37:52 +0600

    fibonacci file added

---
+++ fibonacci.py
@@ -0,0 +1,23 @@
+class Fibonacci:
+
+    def __init__(self,n):
+        ans = self.get_solution(n)
+
+    def get_solution(self,n):
+        a = 0
+        b = 1
+        if n < 0: 
+            print("Incorrect input") 
+        elif n == 0: 
+            return a 
+        elif n == 1: 
+            print(b) 
+        else: 
+            for i in range(2,n+1): 
+                c = a + b 
+                a,b = b,c
+            print(b)
+
+if __name__ == "__main__":
+    n = int(input())
+    fibb = Fibonacci(n)
